f187066f1e2faacded94fcb801f0dd85039f3679
selminiespm <antonio.selmini@espm.br>
2022-03-11 12:20:32 -0300

    minha primeira alteração

---
+++ TesteListaDupla/src/Teste.java
@@ -0,0 +1,13 @@
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		Lista lista = new Lista();
+		
+		lista.inserir("selmini", 123);
+		lista.inserir("sandra", 456);
+		
+		lista.imprimir();
+
+	}
+}
